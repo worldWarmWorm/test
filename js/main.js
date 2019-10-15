87eeec2c7e7863afcfec775849610ff1ad1e6bcb
Valery Davydkin <st.valeriq@gmail.com>
2019-10-15 22:14:02 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+
+    //  portfolio
+
+    $('.button[filter]').click(function () {
+        if ($(this).attr('val') == 'off') {
+            $('.button[filter]').attr('val', 'off').removeClass('focused');
+            $(this).attr('val', 'on').addClass('focused');
+            $('.filter > div').hide(300);
+            $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+            if ($(this).attr('filter') == 'all') {
+                $('.button[filter]').attr('val', 'off').removeClass('focused');
+                $(this).attr('val', 'on').addClass('focused');
+                $('.filter > div').show(300);
+            }
+        }
+    })
+
+    // slider
+
+    $('.multiple-items').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots-style',
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 774,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+
+    // menu
+
+    $('ul.menu a[href^="#"').click(function () {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({ 'color': '#212121' });
+        return false;
+    })
+
+    // drop-down menu
+
+    $('.menu-icon').click(function () {
+        $('nav').slideToggle(500);
+        $('ul.menu').css({
+            'display': 'flex',
+            'flex-direction': 'column'
+        });
+        if ($('.menu-icon').html() == '<i class="fas fa-times"></i>') {
+            $(this).html('<i class="fas fa-bars"></i>');
+        } else {
+            $(this).html('<i class="fas fa-times"></i>');
+        }
+    });
+
+    // to top button
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0)
+            $('#toTop').fadeIn();
+        else
+            $('#toTop').fadeOut();
+    })
+    $('#toTop').click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+    })
+
+})
